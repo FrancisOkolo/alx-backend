0ba0054f1a2b4d37146e5fb79bf344aa7064a9e0
FrancisOkolo <105055795+FrancisOkolo@users.noreply.github.com>
2023-10-23 22:30:04 +0100

    helper function

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -8,7 +8,7 @@ from typing import Tuple
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     '''simple helper function'''
-    
+
     last_obj = page * page_size
     first_obj = last_obj - page_size
     return first_obj, last_obj
